4014a653b598e5cdf3689fa111c9c50433c62a70
Brill-Prime <brillprimeltd@gmail.com>
2025-10-18 11:02:43 +0100

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -6,14 +6,14 @@ import { getStorage } from 'firebase/storage';
 import { getAnalytics } from 'firebase/analytics';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBAe9x-nOCzn8VA1oAP23y2Sv2sIiVyP0s",
-    authDomain: "brillprimefirebase.firebaseapp.com",
-    databaseURL: "https://brillprimefirebase-default-rtdb.firebaseio.com",
-    projectId: "brillprimefirebase",
-    storageBucket: "brillprimefirebase.firebasestorage.app",
-    messagingSenderId: "655201684400",
-    appId: "1:655201684400:web:ec3ac485a4f98a82fb2475",
-    measurementId: "G-7T5QKZPDWW"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
